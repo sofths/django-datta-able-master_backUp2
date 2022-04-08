44208e703f52120eec156b69445fb2db829cddba
sofths <softj0106@gmail.com>
2022-04-08 17:03:18 +0900

    Initial commit

---
+++ django-datta-able-master/apps/authentication/models.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from forms import SignUpForm
+
+
+# def UserInfo(SignUpForm):
+#     print(SignUpForm.company)
+#         return SignUpForm.company
---
+++ django-datta-able-master/apps/authentication/views.py
@@ -0,0 +1,68 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, SignUpForm
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
+
+
+def register_user(request):
+    msg = None
+    success = False
+    print("register 진입")
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            company = form.cleaned_data.get("company")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password,company=company)
+
+            msg = 'User created - please <a href="/login">login</a>.'
+            success = True
+
+            # return redirect("/login/")
+
+        else:
+            msg = 'Form is not valid'
+    else:
+        form = SignUpForm()
+
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success,})
+
+def Users(request):
+    print("Users 진입")
+    if request.method == "GET":
+        form = SignUpForm(request.GET)
+        print("get 진입")
+        return render(request,'test.html')
+    elif request.method == "POST":
+        form = SignUpForm(request.POST)
+        print("POST 진입")
+        return render(request,'test.html')
